033955e8e3b6962b11297b6be3b322621766bcc4
FMSilverio <filipe.m.silverio@hotmail.com>
2020-02-04 15:26:13 +0000

    [FS 20190402] - Display a Post Details Page

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -5,6 +5,8 @@ import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -25,6 +27,12 @@ public class BlogController {
         return "home";
     }
 
+    @RequestMapping("/post/{id}")
+    public String postDetails(@PathVariable Long id, ModelMap modelMap){
+        Post post = postRepository.findById(id);
+        modelMap.put("post", post);
+        return "post-details";
+    }
 
 
 
