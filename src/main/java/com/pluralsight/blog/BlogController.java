d527d64b1761458e8d66f0f23abfc5250fd57fdd
FMSilverio <filipe.m.silverio@hotmail.com>
2020-02-04 15:31:57 +0000

    [FS 20190402] - Display a Post Details Page

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -29,6 +29,8 @@ public class BlogController {
 
     @RequestMapping("/post/{id}")
     public String postDetails(@PathVariable Long id, ModelMap modelMap){
+        Post post = postRepository.findById(id);
+        modelMap.put("post", post);
         return "post-details";
     }
 
